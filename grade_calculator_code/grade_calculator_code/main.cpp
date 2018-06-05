35e0b89677e6e68974933d2f447ea17ad15d6a17
Sophia Luo <sophialuo410@gmail.com>
2018-06-04 19:45:42 -0700

    So... I've always thought that for each grading scheme I would have to write everything into a file, then read it later when I want to calculate. But what if I can just like.. create an object?... I'm gonna code something up that works in console, then transfer to Qt

---
+++ grade_calculator_code/grade_calculator_code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grade_calculator_code
+//
+//  Created by Sophia Luo on 6/4/18.
+//  Copyright © 2018 Sophia Luo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
